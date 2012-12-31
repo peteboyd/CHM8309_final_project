2ddb48b7fcff601d1ec9359c2f9fb2e82f7dc0cf
pboyd <pboyd085@uottawa.ca>
2012-12-31 14:45:44 -0500

    restructured Atom class and added System class

--- peteMD.py
+++ peteMD.py
@@ -13,53 +13,54 @@ class Atom(object):
 
     """
 
-    def __init__(self, element, position=np.zeros(3), name=None,
-                 dimensions=np.matrix(np.identity(3))):
+    def __init__(self, element, position=np.zeros(3), name=None):
         """Read in element symbol. Mass is assigned from a lookup table."""
         self.index = 0  # index can be assigned from some wrapper class
         self.name = name if name else element
         self.position_history = []
-        # dimensions are the system cell dimensions, initially set to 
-        # [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-        self._dimensions = np.matrix(dimensions)
         self._element = element
         self._mass = MASS[element]
         self.set_position(position)
+        # scaled a, b, and c are the fractional positions of the atom
+        # in a box.
+        self._scaled_a = 0.
+        self._scaled_b = 0.
+        self._scaled_c = 0.
 
     @property
     def mass(self):
         return self._mass
 
-    def get_position(self):
-        """Returns the cartesian position of the atom."""
-        return np.array(np.dot(self.dimensions, self._position)).flatten()
-
     def set_position(self, position):
         """Assigns the scaled position to the atom, based on the dimensions
-        of the cell. Raises a ValueError if assignment is attempted after
-        initialization of MD simulation.
+        of the cell.  
+        Each cartesian coordinate is stored as a separate private variable,
+        which makes for easier readability.
 
         """
         if self.position_history:
             raise ValueError("Cannot assign positions once \
                               MD simulation has begun.")
-        position = np.array(position)
-        scaled_position = np.array(np.dot(self._dimensions.I, position)).flatten()
-        self._position = scaled_position - np.floor(scaled_position)
+        self._x = position[0]
+        self._y = position[1]
+        self._z = position[2]
+
+    def get_position(self):
+        """Returns the position as a numpy array."""
+        return np.array([self._x, self._y, self._z])
 
     position = property(get_position, set_position)
 
-    def set_dimensions(self, dimensions):
-        """Assigns the matrix dimensions and converts the position to internal
-        coordinates.
+class System(object):
+    """Contains atoms and system-wide thermodynamic data such as Temperature
+    and periodic boundaries, etc..
 
+    """
+
+    def __init__(self, temperature, boundaries):
+        """Initialize the MD system with a temperature and periodic boundaries.
+        
         """
-        curr_pos = self.get_position()
-        self._dimensions = np.matrix(dimensions)
-        self.set_position(curr_pos)
-
-    def get_dimensions(self):
-        return self._dimensions
-
-    dimensions = property(get_dimensions, set_dimensions)
+        self.temperature = temperature
+        self.boundaries = boundaries
 
